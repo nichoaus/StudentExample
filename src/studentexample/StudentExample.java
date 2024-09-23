e133edf7244032aaf747110bbda3d3cf5f8e8615
nichoaus <nichoaus@sheridancollege.ca>
2024-09-23 12:45:29 -0400

    Hello, this is Step 1

---
+++ src/studentexample/StudentExample.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentexample;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acsni
+ */
+public class StudentExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        Student[] students = new Student[5];
+        
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("Enter a name");
+            String name = input.nextLine();
+            students[i] = new Student(name);            
+        }
+        
+        students[2].setAge(18);
+        
+        for (Student student: students) {
+            System.out.printf("Student %s is %d years old\n", 
+                    student.getName(), student.getAge());
+        }
+    }
+    
+}
